0fcf444f6ee04ac8c86a5a4bdf7d65de96278ce8
Merve Rabia Barın <35612610+mrabiabrn@users.noreply.github.com>
2023-12-26 17:52:32 +0300

    Add metrics table

--- metrics.py
+++ metrics.py
@@ -22,4 +22,20 @@ def R2(predictions, labels):
     return 1 - np.sum((predictions - labels)**2) / np.sum((labels - np.mean(labels))**2)
 
 
+# given a list of predictions and a list of labels, return a table of metrics
+def metrics_table(predictions, labels):
+    mse = MSE(predictions, labels)
+    mae = MAE(predictions, labels)
+    rmse = RMSE(predictions, labels)
+    mape = MAPE(predictions, labels)
+    r2 = R2(predictions, labels)
+
+    return f'+-------+-------+-------+-------+-------+\n' \
+           f'|  MSE  |  MAE  |  RMSE |  MAPE |   R2  |\n' \
+           f'+-------+-------+-------+-------+-------+\n' \
+           f'| {mse:.3f} | {mae:.3f} | {rmse:.3f} | {mape:.3f} | {r2:.3f} |\n' \
+           f'+-------+-------+-------+-------+-------+'
+
+
+
 
